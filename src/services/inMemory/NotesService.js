77f5bab0db904ecdebc3ec6dd64bfeba3646295d
theamik69 <amishbah12@gmail.com>
2023-04-26 16:03:12 +0700

    data validation implemented

--- src/services/inMemory/NotesService.js
+++ src/services/inMemory/NotesService.js
@@ -1,4 +1,6 @@
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 
 class NotesService {
   constructor() {
@@ -9,19 +11,19 @@ class NotesService {
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
- 
+
     const newNote = {
       title, tags, body, id, createdAt, updatedAt,
     };
- 
+
     this._notes.push(newNote);
 
     const isSuccess = this._notes.filter((note) => note.id === id).length > 0;
 
     if (!isSuccess) {
-      throw new Error('Catatan gagal ditambahkan');
+      throw new InvariantError('Catatan gagal ditambahkan');
     }
- 
+
     return id;
   }
 
@@ -33,20 +35,20 @@ class NotesService {
     const note = this._notes.filter((n) => n.id === id)[0];
 
     if (!note) {
-        throw new Error('Catatan tidak ditemukan');
-      }
-      return note;
+      throw new NotFoundError('Catatan tidak ditemukan');
+    }
+    return note;
   }
 
   editNoteById(id, { title, body, tags }) {
     const index = this._notes.findIndex((note) => note.id === id);
- 
+
     if (index === -1) {
-      throw new Error('Gagal memperbarui catatan. Id tidak ditemukan');
+      throw new NotFoundError('Gagal memperbarui catatan. Id tidak ditemukan');
     }
- 
+
     const updatedAt = new Date().toISOString();
- 
+
     this._notes[index] = {
       ...this._notes[index],
       title,
@@ -59,11 +61,10 @@ class NotesService {
   deleteNoteById(id) {
     const index = this._notes.findIndex((note) => note.id === id);
     if (index === -1) {
-      throw new Error('Catatan gagal dihapus. Id tidak ditemukan');
+      throw new NotFoundError('Catatan gagal dihapus. Id tidak ditemukan');
     }
     this._notes.splice(index, 1);
   }
-
 }
 
 module.exports = NotesService;
